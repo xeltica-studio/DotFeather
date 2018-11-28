b084a3803030569d0435029027072f96b0fb5873
Xeltica <citringo@gmail.com>
2018-11-29 00:49:04 +0900

    サンプル

--- DotFeather.Test.NetCore/Game.cs
+++ DotFeather.Test.NetCore/Game.cs
@@ -6,12 +6,29 @@ namespace DotFeather.Test.NetCore
 	{
 		public Game(int width, int height, string title = null, int refreshRate = 60) : base(width, height, title, refreshRate)
 		{
-
+			this.title = Title;
 		}
+
+		string title;
 
 		static void Main(string[] args)
 		{
 			new Game(640, 480).Run();
+		}
+
+		protected override void OnUpdate(object sender, DFEventArgs e)
+		{
+			if (Input.Keyboard.Space.IsPressed)
+				Exit(0);
+
+			if (Input.Keyboard.F1.IsPressed)
+				title = "ねこ";
+			if (Input.Keyboard.F2.IsPressed)
+				title = "いぬ";
+			if (Input.Keyboard.F3.IsPressed)
+				title = "さる";
+
+			Title = $"{title} - {(int)(1 / e.DeltaTime + .5)}fps";
 		}
 	}
 }

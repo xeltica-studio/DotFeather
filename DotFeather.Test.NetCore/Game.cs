d88f7786faba5b43038a228cac8b5a21e9bf5194
Xeltica <citringo@gmail.com>
2018-11-30 19:06:27 +0900

    テストで猫が歩くように

--- DotFeather.Test.NetCore/Game.cs
+++ DotFeather.Test.NetCore/Game.cs
@@ -19,12 +19,23 @@ namespace DotFeather.Test.NetCore
 		GraphicLayer g;
 
 		bool isCatMode = false;
+		Texture2D[] catTexture;
+		//readonly TextureDrawable catDrawable;
+
+		Vector2 nekoPos;
+
+		Vector2 nekoVel;
+
+		int ground;
+
 
 		public Game(int width, int height, string title = null, int refreshRate = 60) : base(width, height, title, refreshRate)
 		{
 			this.title = Title;
 			g = new GraphicLayer();
 			this.Layers.Add(g);
+			nekoPos = new Vector2(width / 2, height / 2);
+			ground = height - 96;
 		}
 
 		protected override void OnLoad(object sender, EventArgs e)
@@ -34,6 +45,7 @@ namespace DotFeather.Test.NetCore
 			g.Line(0, 120, 320, 120, Color.Lime);
 
 			g.Rect(400, 320, 432, 352, Color.Cyan);
+			catTexture = LoadDividedImage("cat.png", 4, 1, new Size(16, 24));
 		}
 
 		protected override void OnUpdate(object sender, DFEventArgs e)
@@ -41,17 +53,17 @@ namespace DotFeather.Test.NetCore
 			if (Input.Keyboard.Space.IsPressed)
 				Exit(0);
 
-			if (Input.Keyboard.F1.IsPressed)
+			if (Input.Keyboard.Number1.IsPressed)
 			{
 				title = "ねこ";
 				g.Clear();
 				isCatMode = true;
 			}
-			if (Input.Keyboard.F2.IsPressed)
+			if (Input.Keyboard.Number2.IsPressed)
 				title = "いぬ";
-			if (Input.Keyboard.F3.IsPressed)
+			if (Input.Keyboard.Number3.IsPressed)
 				title = "さる";
-			if (Input.Keyboard.F4.IsPressed)
+			if (Input.Keyboard.Number4.IsPressed)
 			{
 				Width = Random.Next(64, 256);
 				Height = Random.Next(64, 256);
@@ -59,11 +71,28 @@ namespace DotFeather.Test.NetCore
 
 			if (isCatMode)
 			{
+				g.Clear();
+				var tex = catTexture[DateTime.Now.Millisecond % 8 < 4 ? 0 : 1];
+				nekoVel.X = Input.Keyboard.Left.IsPressed ? -2 : Input.Keyboard.Right.IsPressed ? 2 : 0;
 
+				if (nekoVel.X != 0) 
+					tex = catTexture[0];
+
+				nekoVel.Y = (int)(nekoVel.Y + 9.8 * e.DeltaTime);
+
+
+				if (nekoPos.Y >= ground && nekoVel.Y > 0)
+				{
+					nekoPos.Y = ground;
+					nekoVel.Y = 0;
+				}
+				nekoPos += nekoVel;
+
+				g.Texture(nekoPos.X, nekoPos.Y, tex);
 			}
 
 
-			Title = $"{title} - {(int)(1 / e.DeltaTime + .5)}fps";
+			Title = $"{Input.Mouse.Position} - {(int)(1 / e.DeltaTime + .5)}fps";
 		}
 	}
 }

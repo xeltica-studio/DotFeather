a31deb277cc190a20ee55824777b28da9962f2fd
Xeltica <citringo@gmail.com>
2018-12-04 14:38:50 +0900

    サンプルコード編集

--- DotFeather.Test.NetCore/Game.cs
+++ DotFeather.Test.NetCore/Game.cs
@@ -28,6 +28,8 @@ namespace DotFeather.Test.NetCore
 
 		int ground;
 
+		bool prevSpace;
+
 
 		public Game(int width, int height, string title = null, int refreshRate = 60) : base(width, height, title, refreshRate)
 		{
@@ -40,33 +42,42 @@ namespace DotFeather.Test.NetCore
 
 		protected override void OnLoad(object sender, EventArgs e)
 		{
+			DrawLines();
+			catTexture = LoadDividedImage("cat.png", 4, 1, new Size(16, 24));
+		}
+
+		void DrawLines()
+		{
+			g.Clear();
 			g.Line(0, 0, 320, 240, Color.Red);
 			g.Line(320, 0, 0, 240, Color.Blue);
 			g.Line(0, 120, 320, 120, Color.Lime);
 
 			g.Rect(400, 320, 432, 352, Color.Cyan);
-			catTexture = LoadDividedImage("cat.png", 4, 1, new Size(16, 24));
 		}
 
 		protected override void OnUpdate(object sender, DFEventArgs e)
 		{
-			if (Input.Keyboard.Space.IsPressed)
+			if (Input.Keyboard.Escape.IsPressed)
 				Exit(0);
 
 			if (Input.Keyboard.Number1.IsPressed)
 			{
 				title = "ねこ";
 				g.Clear();
+				nekoPos = nekoVel = default;
 				isCatMode = true;
 			}
 			if (Input.Keyboard.Number2.IsPressed)
-				title = "いぬ";
-			if (Input.Keyboard.Number3.IsPressed)
-				title = "さる";
+			{
+				title = "線描画";
+				DrawLines();
+				isCatMode = false;
+			}
 			if (Input.Keyboard.Number4.IsPressed)
 			{
-				Width = Random.Next(64, 256);
-				Height = Random.Next(64, 256);
+				Width = Random.Next(640, 1280);
+				Height = Random.Next(480, 720);
 			}
 
 			if (isCatMode)
@@ -75,24 +86,34 @@ namespace DotFeather.Test.NetCore
 				var tex = catTexture[DateTime.Now.Millisecond % 8 < 4 ? 0 : 1];
 				nekoVel.X = Input.Keyboard.Left.IsPressed ? -2 : Input.Keyboard.Right.IsPressed ? 2 : 0;
 
-				if (nekoVel.X != 0) 
+				if ((int)(nekoVel.X + .5) == 0) 
 					tex = catTexture[0];
 
-				nekoVel.Y = (int)(nekoVel.Y + 9.8 * e.DeltaTime);
+				nekoVel.Y = nekoVel.Y + 9.8f * (float)e.DeltaTime;
 
+				if (Input.Keyboard.Space.IsPressed && !prevSpace)
+				{
+					nekoVel.Y = -8;
+				}
 
 				if (nekoPos.Y >= ground && nekoVel.Y > 0)
 				{
 					nekoPos.Y = ground;
 					nekoVel.Y = 0;
 				}
+				else
+				{
+					tex = catTexture[2];
+				}
 				nekoPos += nekoVel;
 
-				g.Texture(nekoPos.X, nekoPos.Y, tex);
+
+				g.Texture((int)nekoPos.X, (int)nekoPos.Y, tex);
+				prevSpace = Input.Keyboard.Space.IsPressed;
 			}
 
 
-			Title = $"{Input.Mouse.Position} - {(int)(1 / e.DeltaTime + .5)}fps";
+			Title = $"{title} - {(int)(1 / e.DeltaTime + .5)}fps";
 		}
 	}
 }

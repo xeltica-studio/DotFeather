aced9d173ad4a7ab657cb58e50bcaa31891b6474
Xeltica <citringo@gmail.com>
2019-04-04 00:13:56 +0900

    Container を IList の実装にし、 Children プロパティを削除した

--- DotFeather/Drawable/Container.cs
+++ DotFeather/Drawable/Container.cs
@@ -1,9 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace DotFeather.Drawable
 {
-	public class Container : IDrawable
+	public class Container : IDrawable, IList<IDrawable>
 	{
 		public int ZOrder { get; set; }
 		public string Name { get; set; }
@@ -11,14 +12,43 @@ namespace DotFeather.Drawable
 		public float Angle { get; set; }
 		public Vector Scale { get; set; }
 
-		public List<IDrawable> Children { get; } = new List<IDrawable>(10000);
+		private List<IDrawable> Children { get; } = new List<IDrawable>(10000);
+
+		public IDrawable this[int index]
+		{
+			get => Children[index];
+			set => Children[index] = value;
+		}
 
 		public void Draw(GameBase game, Vector location)
 		{
-			Children.Sort((d1, d2) => d1.ZOrder < d2.ZOrder ? - 1 : d1.ZOrder > d2.ZOrder ? 1 : 0);
 			foreach (var child in Children.ToList())
 				child.Draw(game, Location + location);
 		}
-	}
 
+		public void Sort() =>
+			Children.Sort((d1, d2) => d1.ZOrder < d2.ZOrder ? -1 : d1.ZOrder > d2.ZOrder ? 1 : 0);
+
+		public void Add(IDrawable child)
+		{
+			Sort();
+			Children.Add(child);
+		}
+
+		public int IndexOf(IDrawable item) => Children.IndexOf(item);
+		public void Insert(int index, IDrawable item)
+		{
+			Sort();
+			Children.Insert(index, item);
+		}
+		public void RemoveAt(int index) => Children.RemoveAt(index);
+		public void Clear() => Children.Clear();
+		public bool Contains(IDrawable item) => Children.Contains(item);
+		public void CopyTo(IDrawable[] array, int arrayIndex) => Children.CopyTo(array, arrayIndex);
+		public bool Remove(IDrawable item) => Children.Remove(item);
+		public IEnumerator<IDrawable> GetEnumerator() => Children.GetEnumerator();
+		IEnumerator IEnumerable.GetEnumerator() => Children.GetEnumerator();
+		public int Count => Children.Count;
+		public bool IsReadOnly => false;
+	}
 }

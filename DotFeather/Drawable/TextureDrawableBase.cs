19cd3dbcacc2d3973ca4d681956503adfb528853
Xeltica <citringo@gmail.com>
2019-04-28 19:03:17 +0900

    TextureDrawableBase

---
+++ DotFeather/Drawable/TextureDrawableBase.cs
@@ -0,0 +1,39 @@
+﻿namespace DotFeather
+{
+	/// <summary>
+	/// <see cref="Texture2D"/> を描画するオブジェクトの抽象クラスです。
+	/// </summary>
+    public abstract class TextureDrawableBase : IDrawable
+    {
+		/// <summary></summary>
+        public virtual int ZOrder { get; set; }
+        /// <summary></summary>
+        public virtual string Name { get; set; }
+        /// <summary></summary>
+        public virtual Vector Location { get; set; }
+        /// <summary></summary>
+        public virtual float Angle { get; set; }
+        /// <summary></summary>
+        public virtual Vector Scale { get; set; }
+
+		/// <summary>
+		/// テクスチャを取得します。
+		/// </summary>
+		public virtual Texture2D Texture { get; protected set; }
+
+		/// <summary>
+		/// このオブジェクトを破棄します。
+		/// </summary>
+        public virtual void Destroy()
+        {
+        }
+
+		/// <summary>
+		/// 描画します。
+		/// </summary>
+        public virtual void Draw(GameBase game, Vector location)
+        {
+            TextureDrawer.Draw(game, Texture, location + Location, Scale, Angle);
+        }
+    }
+}
--- DotFeather/Sprite.cs
+++ DotFeather/Sprite.cs
@@ -1,94 +1,83 @@
-#pragma warning disable RECS0018 // 等値演算子による浮動小数点値の比較
+﻿#pragma warning disable RECS0018 // 等値演算子による浮動小数点値の比較
 
 
 namespace DotFeather
 {
-	/// <summary>
-	/// テクスチャを描画する <see cref="IDrawable"/> オブジェクトです。
-	/// </summary>
-	public class Sprite : IDrawable
-	{
-		/// <summary>
-		/// この <see cref="Sprite"/> が持つテクスチャを取得または設定します。
-		/// </summary>
-		/// <value></value>
-		public Sprite(Texture2D texture, Vector location, float angle, Vector scale, int zOrder, string name)
-		{
-			this.Texture = texture;
-				this.Location = location;
-				this.Angle = angle;
-				this.Scale = scale;
-				this.ZOrder = zOrder;
-				this.Name = name;
-
-		}
-
-		/// <summary>
-		/// このスプライトが持つテクスチャを取得または設定します。
-		/// </summary>
-		/// <value></value>
-		public Texture2D Texture { get; set; }
-
-		/// <summary></summary>
-		public Vector Location { get; set; }
-
-		/// <summary></summary>
-		public float Angle { get; set; }
-
-		/// <summary></summary>
-		public Vector Scale { get; set; }
-
-		/// <summary></summary>
-		public int ZOrder { get; set; }
-
-		/// <summary></summary>
-		public string Name { get; set; }
-
-		private Texture2D internalTexture;
-
-		/// <summary>
-		/// <see cref="Sprite"/> クラスの新しいインスタンスを初期化します。
-		/// </summary>
-		/// <param name="texture">この <see cref="Sprite"/> が使用するテクスチャ。</param>
-		/// <param name="x">初期位置 X。</param>
-		/// <param name="y">初期位置 Y。</param>
-		/// <param name="angle">初期角度。</param>
-		/// <param name="scale">初期スケール</param>
-		public Sprite(Texture2D texture, int x, int y, float angle = default, Vector scale = default)
-			: this(texture)
-		{
-			Location = new Vector(x, y);
-			Angle = angle;
-			Scale = scale != default ? scale : new Vector(1, 1);
-		}
-
-		/// <summary>
+    /// <summary>
+    /// テクスチャを描画する <see cref="IDrawable"/> オブジェクトです。
+    /// </summary>
+    public class Sprite : TextureDrawableBase
+    {
+        /// <summary>
         /// <see cref="Sprite"/> クラスの新しいインスタンスを初期化します。
+        /// </summary>
+        /// <value></value>
+        public Sprite(Texture2D texture, Vector location, float angle, Vector scale, int zOrder, string name)
+        {
+            this.Texture = texture;
+            this.Location = location;
+            this.Angle = angle;
+            this.Scale = scale;
+            this.ZOrder = zOrder;
+            this.Name = name;
+        }
+
+		/// <summary>
+		/// この <see cref="Sprite"/> が描画するテクスチャを取得または設定します。
 		/// </summary>
+		/// <value></value>
+        public new Texture2D Texture
+        {
+            get => base.Texture;
+            set => base.Texture = value;
+        }
+
+        /// <summary>
+        /// <see cref="Sprite"/> クラスの新しいインスタンスを初期化します。
+        /// </summary>
+        /// <param name="texture">この <see cref="Sprite"/> が使用するテクスチャ。</param>
+        /// <param name="x">初期位置 X。</param>
+        /// <param name="y">初期位置 Y。</param>
+        /// <param name="angle">初期角度。</param>
+        /// <param name="scale">初期スケール</param>
+        public Sprite(Texture2D texture, int x, int y, float angle = default, Vector scale = default)
+			: this(texture)
+        {
+            Location = new Vector(x, y);
+            Angle = angle;
+            Scale = scale != default ? scale : new Vector(1, 1);
+        }
+
+        /// <summary>
+        /// <see cref="Sprite"/> クラスの新しいインスタンスを初期化します。
+        /// </summary>
         /// <param name="texture">この <see cref="Sprite"/> が使用するテクスチャ。</param>
         public Sprite(Texture2D texture)
-		{
+        {
             Texture = texture;
 		}
 
-		/// <summary>
-		/// 指定した画像ファイルから <see cref="Sprite"/> を生成します。
-		/// </summary>
-		/// <param name="path">ファイルパス。</param>
-		/// <returns>生成された <see cref="Sprite"/>。</returns>
-		public static Sprite LoadFrom(string path) => new Sprite(path);
+        /// <summary>
+        /// 指定した画像ファイルから <see cref="Sprite"/> を生成します。
+        /// </summary>
+        /// <param name="path">ファイルパス。</param>
+        /// <returns>生成された <see cref="Sprite"/>。</returns>
+        public static Sprite LoadFrom(string path) => new Sprite(path);
 
-		/// <summary>
-		/// この <see cref="Sprite"/> を破棄します。
-		/// </summary>
-		public void Destroy()
-		{
-			internalTexture.Dispose();
-		}
+        /// <summary>
+        /// この <see cref="Sprite"/> を破棄します。
+        /// </summary>
+        public override void Destroy()
+        {
+            internalTexture.Dispose();
+			base.Destroy();
+        }
 
-		private Sprite(string path)
-		{
-			Texture = internalTexture = Texture2D.LoadFrom(path);
-		}
-	}
+        private Sprite(string path)
+        {
+            Texture = internalTexture = Texture2D.LoadFrom(path);
+        }
+
+        private Texture2D internalTexture;
+    }
 }

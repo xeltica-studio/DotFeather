fff6b78a94efc85a1f420cab4935721d7e6ed773
Xeltica <citringo@gmail.com>
2019-05-04 22:35:58 +0900

    キーボード API の機能拡張

--- DotFeather/InputSystems/DFKey.cs
+++ DotFeather/InputSystems/DFKey.cs
@@ -12,11 +12,29 @@ namespace DotFeather
 			source = key;
 		}
 
-		/// <summary>
-		/// キーが押されているかどうかを取得します。
-		/// </summary>
-		/// <value><c>true</c> であれば押されています。<c>false</c> であれば押されていません。</value>
-		public bool IsPressed => Keyboard.GetState()[source];
+        /// <summary>
+        /// キーが押されているかどうかを示す値を取得します。
+        /// </summary>
+        /// <value><c>true</c> であれば押されています。<c>false</c> であれば押されていません。</value>
+        public bool IsPressed => Keyboard.GetState()[source];
+
+        /// <summary>
+        /// キーがこのフレームで押されたかどうかを示す値を取得します。
+        /// </summary>
+        /// <returns><c>true</c> であればたった今押されました。<c>false</c> であればそうでありません。</returns>
+        public bool IsKeyDown => Keyboard.GetState().IsKeyDown(source);
+
+        /// <summary>
+        /// キーがこのフレームで離されたかどうかを示す値を取得します。
+        /// </summary>
+        /// <returns><c>true</c> であればたった今離されました。<c>false</c> であればそうでありません。</returns>
+        public bool IsKeyUp => Keyboard.GetState().IsKeyUp(source);
+
+        /// <summary>
+        /// キーが押されているかどうかを取得します。
+        /// </summary>
+        /// <value><c>true</c> であれば押されています。<c>false</c> であれば押されていません。</value>
+        public static implicit operator bool(DFKey key) => key.IsPressed;
 
 		private readonly Key source;
 	}

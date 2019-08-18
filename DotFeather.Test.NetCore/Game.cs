c615d5908caea0299962f3acdc9ed2ae8e4b655f
Xeltica <xeltica@gmail.com>
2019-08-19 02:47:12 +0900

    update sample

--- DotFeather.Test.NetCore/Game.cs
+++ DotFeather.Test.NetCore/Game.cs
@@ -17,50 +17,22 @@ namespace DotFeather
 
 		protected override void OnLoad(object sender, EventArgs e)
 		{
-			Root.Add(g);
-			for (var i = 0; i < 100; i++)
-			{
-				int x = rnd.Next(1000), y = rnd.Next(1000);
-				g.Rect(x, y, x + rnd.Next(256), y + rnd.Next(256), rnd.NextColor(), rnd.Next(32), rnd.NextColor());
-			}
+			red = new Sprite(Texture2D.CreateSolid(Color.FromArgb(0x7fff0000), 256, 256));
+			green = new Sprite(Texture2D.CreateSolid(Color.FromArgb(0x7f00ff00), 256, 256));
 
-			StartCoroutine(Coroutine1())
-				.Then(a => StartCoroutine(Coroutine2(a))
-					.Error(err => Console.WriteLine($"{err.GetType().Name}: {err.Message}\n{err.StackTrace}")));
+			red.Location = new Vector(64, 64);
+			green.Location = new Vector(192, 192);
+
+			Root.Add(green);
+			Root.Add(red);
 		}
 
 		protected override void OnUpdate(object sender, DFEventArgs e)
 		{
 			if (Input.Keyboard.Escape.IsKeyUp)
 				Exit(0);
-			var clicked = Input.Mouse.IsLeft;
-			var mp = Input.Mouse.Position;
-			if (p is Point pp)
-			{
-				g.Location += new Vector(mp.X - pp.X, mp.Y - pp.Y);
-			}
-			p = clicked ? mp : (Point?)null;	
         }
 
-		IEnumerator Coroutine1()
-		{
-			for (var i = 0; i < 120; i++)
-			{
-				Console.WriteLine(i);
-				yield return i;
-			}
-		}
-
-		IEnumerator Coroutine2(object a)
-		{
-			Console.WriteLine(a);
-			yield return new WaitForSeconds(5);
-			throw new Exception("Sample Error");
-		}
-
-		Graphic g = new Graphic();
-
-		private Random rnd = new Random();
-		private Point? p;
+		Sprite red, green;
     }
 }

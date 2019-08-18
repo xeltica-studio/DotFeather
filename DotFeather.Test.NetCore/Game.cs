d78acbb9589f9d1bff20aee19151a83935c7c455
Xeltica <xeltica@gmail.com>
2019-08-19 01:50:42 +0900

    Update Sample

--- DotFeather.Test.NetCore/Game.cs
+++ DotFeather.Test.NetCore/Game.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Text;
@@ -16,56 +17,50 @@ namespace DotFeather
 
 		protected override void OnLoad(object sender, EventArgs e)
 		{
-			var fnt = new Font(new FontFamily("ヒラギノ角ゴ Std"), 40);
-			red = new TextDrawable("ちくわぶ\nねこ", fnt, Color.White);
-			green = new TextDrawable("かに", fnt, Color.Aquamarine);
-			Root.Add(red);
-			Root.Add(green);
-			red.Location = new Vector(Width / 4 - red.Width / 2, Height / 2 - red.Height / 2);
-			green.Location = new Vector(Width / 4 * 3 - green.Width / 2, Height / 2 - green.Height / 2);
+			Root.Add(g);
+			for (var i = 0; i < 100; i++)
+			{
+				int x = rnd.Next(1000), y = rnd.Next(1000);
+				g.Rect(x, y, x + rnd.Next(256), y + rnd.Next(256), rnd.NextColor(), rnd.Next(32), rnd.NextColor());
+			}
+
+			StartCoroutine(Coroutine1())
+				.Then(a => StartCoroutine(Coroutine2(a))
+					.Error(err => Console.WriteLine($"{err.GetType().Name}: {err.Message}\n{err.StackTrace}")));
 		}
 
 		protected override void OnUpdate(object sender, DFEventArgs e)
 		{
 			if (Input.Keyboard.Escape.IsKeyUp)
 				Exit(0);
-			
-			if (Input.Keyboard.Space.IsKeyDown)
+			var clicked = Input.Mouse.IsLeft;
+			var mp = Input.Mouse.Position;
+			if (p is Point pp)
 			{
-				BackgroundColor = BackgroundColor == Color.Black ? Color.White : Color.Black;
+				g.Location += new Vector(mp.X - pp.X, mp.Y - pp.Y);
 			}
-			const int speed = 256;
-			if (Input.Keyboard.W)
-				red.Location += new Vector(0, -1) * (float)(speed * e.DeltaTime);
-			if (Input.Keyboard.A)
-				red.Location += new Vector(-1, 0) * (float)(speed * e.DeltaTime);
-			if (Input.Keyboard.S)
-				red.Location += new Vector(0, 1) * (float)(speed * e.DeltaTime);
-			if (Input.Keyboard.D)
-				red.Location += new Vector(1, 0) * (float)(speed * e.DeltaTime);
-
-			if (Input.Keyboard.Up)
-				green.Location += new Vector(0, -1) * (float)(speed * e.DeltaTime);
-			if (Input.Keyboard.Left)
-				green.Location += new Vector(-1, 0) * (float)(speed * e.DeltaTime);
-			if (Input.Keyboard.Down)
-				green.Location += new Vector(0, 1) * (float)(speed * e.DeltaTime);
-			if (Input.Keyboard.Right)
-				green.Location += new Vector(1, 0) * (float)(speed * e.DeltaTime);
-
-			if (Input.Keyboard.Number1)
-				WindowMode = WindowMode.Fixed;
-
-			if (Input.Keyboard.Number2)
-				WindowMode = WindowMode.NoFrame;
-
-			if (Input.Keyboard.Number3)
-				WindowMode = WindowMode.Resizable;
-
-			if (Input.Keyboard.Number4)
-				IsFullScreen = !IsFullScreen;
+			p = clicked ? mp : (Point?)null;	
         }
 
-		private TextureDrawableBase red, green;
+		IEnumerator Coroutine1()
+		{
+			for (var i = 0; i < 120; i++)
+			{
+				Console.WriteLine(i);
+				yield return i;
+			}
+		}
+
+		IEnumerator Coroutine2(object a)
+		{
+			Console.WriteLine(a);
+			yield return new WaitForSeconds(5);
+			throw new Exception("Sample Error");
+		}
+
+		Graphic g = new Graphic();
+
+		private Random rnd = new Random();
+		private Point? p;
     }
 }

145ac87e365f021a992af4e7c33f989c01c1e085
Xeltica <xeltica@gmail.com>
2019-10-24 21:08:52 +0900

    😺

--- DotFeather.Test.NetCore/Game.cs
+++ DotFeather.Test.NetCore/Game.cs
@@ -6,14 +6,14 @@ namespace DotFeather.Example
     class Game : GameBase
 	{
 
-		public Game(int width, int height, string title = null, int refreshRate = 60) : base(width, height, title, refreshRate)
+		public Game(int width, int height, string title = "", int refreshRate = 60) : base(width, height, title, refreshRate)
 		{
 			WindowMode = WindowMode.Resizable;
+			router = new Router(this);
 		}
 
 		protected override void OnLoad(object sender, EventArgs e)
 		{
-			router = new Router(this);
 			router.ChangeScene<LauncherScene>();
 		}
 

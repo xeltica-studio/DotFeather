479e02ad6f61e742856cd18f0161cbc6afa0f632
Xeltica <xeltica@gmail.com>
2019-10-29 18:09:09 +0900

    Add 2 demo apps

--- demo/Scenes/Examples/input/Keyboard.cs
+++ demo/Scenes/Examples/input/Keyboard.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+
 namespace DotFeather.Demo
 {
     [DemoScene("/input/keyboard")]
@@ -7,13 +9,20 @@ namespace DotFeather.Demo
     {
         public override void OnStart(Router router, GameBase game, System.Collections.Generic.Dictionary<string, object> args)
         {
-
+			var head = DemoOS.Text("Keyboard State", 48);
+			head.Location = Vector.One * 16;
+			log.Location = new Vector(16, 32 + head.Height);
+			Root.Add(head);
+			Root.Add(log);
         }
 
         public override void OnUpdate(Router router, GameBase game, DFEventArgs e)
         {
-
+			log.Text = $@"Pressing: {string.Join(",", DFKeyboard.AllPressedKeys.Select(d => d.ToString()))}
+Pressed: {string.Join(",", DFKeyboard.AllDownKeys.Select(d => d.ToString()))}
+Released: {string.Join(",", DFKeyboard.AllUpKeys.Select(d => d.ToString()))}";
         }
-    }
 
+		TextDrawable log = DemoOS.Text("", 16);
+    }
 }
--- demo/Scenes/Examples/input/Mouse.cs
+++ demo/Scenes/Examples/input/Mouse.cs
@@ -7,13 +7,44 @@ namespace DotFeather.Demo
     {
         public override void OnStart(Router router, GameBase game, System.Collections.Generic.Dictionary<string, object> args)
         {
-
+			var head = DemoOS.Text("Mouse State", 48);
+			head.Location = Vector.One * 16;
+			log.Location = new Vector(16, 32 + head.Height);
+			Root.Add(head);
+			Root.Add(log);
         }
 
         public override void OnUpdate(Router router, GameBase game, DFEventArgs e)
         {
-
+			log.Text = $@"Left: {Left()}
+Middle: {Middle()}
+Right: {Right()}
+Scroll: {DFMouse.Scroll}
+Pos: {DFMouse.Position}";
         }
+
+		public string Left()
+		{
+			return DFMouse.IsLeftDown ? "Pressed" :
+				   DFMouse.IsLeftUp ? "Released" :
+				   DFMouse.IsLeft ? "Pressing" : "";
+		}
+
+		public string Middle()
+		{
+			return DFMouse.IsMiddleDown ? "Pressed" :
+				   DFMouse.IsMiddleUp ? "Released" :
+				   DFMouse.IsMiddle ? "Pressing" : "";
+		}
+
+		public string Right()
+		{
+			return DFMouse.IsRightDown ? "Pressed" :
+				   DFMouse.IsRightUp ? "Released" :
+				   DFMouse.IsRight ? "Pressing" : "";
+		}
+
+		TextDrawable log = DemoOS.Text("", 16);
     }
 
 }

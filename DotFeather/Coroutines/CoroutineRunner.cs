6e2dc7359c2c58fe3d38f3b00a12fa1156c4f3dd
Xeltica <xeltica@gmail.com>
2019-11-19 15:25:44 +0900

    Fix コルーチン内でコルーチンを実行できない不具合

--- DotFeather/Coroutines/CoroutineRunner.cs
+++ DotFeather/Coroutines/CoroutineRunner.cs
@@ -5,74 +5,77 @@ using System.Linq;
 
 namespace DotFeather
 {
-	/// <summary>
-	/// A static coroutine manager class.
-	/// </summary>
-	public static class CoroutineRunner
-	{
-		/// <summary>
-		/// Start the specified coroutine.
-		/// </summary>
-		public static Coroutine Start(IEnumerator coroutine)
-		{
-			var c = new Coroutine(coroutine);
+    /// <summary>
+    /// A static coroutine manager class.
+    /// </summary>
+    public static class CoroutineRunner
+    {
+        /// <summary>
+        /// Start the specified coroutine.
+        /// </summary>
+        public static Coroutine Start(IEnumerator coroutine)
+        {
+            var c = new Coroutine(coroutine);
 
-			coroutines[c] = null;
-			c.Start();
-			return c;
-		}
+            coroutines[c] = null;
+            c.Start();
+            return c;
+        }
 
-		/// <summary>
-		/// Stop the specified coroutine.
-		/// </summary>
-		public static void Stop(Coroutine coroutine)
-		{
-			coroutines.Remove(coroutine);
-			coroutine.Stop();
-		}
+        /// <summary>
+        /// Stop the specified coroutine.
+        /// </summary>
+        public static void Stop(Coroutine coroutine)
+        {
+            coroutines.Remove(coroutine);
+            coroutine.Stop();
+        }
 
-		internal static void Update()
-		{
-			foreach (var (coroutine, obj) in coroutines.Select(c => (c.Key, c.Value)).ToArray())
-			{
-				var currentInst = ToYieldInstruction(obj);
+        internal static void Update()
+        {
+            foreach (var (coroutine, obj) in coroutines.Select(c => (c.Key, c.Value)).ToArray())
+            {
+                var currentInst = ToYieldInstruction(obj);
 
-				if (!currentInst.KeepWaiting)
-				{
-					try
-					{
-						if (coroutine.MoveNext())
-						{
-							coroutines[coroutine] = coroutine.Current;
-						}
-						else
-						{
-							Stop(coroutine);
-							coroutine.ThenAction?.Invoke(obj);
-						}
-					}
-					catch (Exception ex)
-					{
-						coroutine.Stop();
-						coroutine.ErrorAction?.Invoke(ex);
-					}
-				}
-			}
-		}
+                if (!currentInst.KeepWaiting)
+                {
+                    try
+                    {
+                        if (coroutine.MoveNext())
+                        {
+                            var cur = coroutine.Current;
+                            // IEnumerator が来たら再度コルーチン開始する
+                            cur = cur is IEnumerator ie ? Start(ie) : cur;
+                            coroutines[coroutine] = cur;
+                        }
+                        else
+                        {
+                            Stop(coroutine);
+                            coroutine.ThenAction?.Invoke(obj);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        coroutine.Stop();
+                        coroutine.ErrorAction?.Invoke(ex);
+                    }
+                }
+            }
+        }
 
-		private static YieldInstruction ToYieldInstruction(object obj)
-		{
-			switch (obj)
-			{
-				case YieldInstruction y:
-					return y;
-				case IEnumerator ie:
-					return CoroutineRunner.Start(ie);
-				default:
-					return new WaitUntilNextFrame();
-			}
-		}
+        private static YieldInstruction ToYieldInstruction(object obj)
+        {
+            switch (obj)
+            {
+                case YieldInstruction y:
+                    return y;
+                case IEnumerator ie:
+                    return CoroutineRunner.Start(ie);
+                default:
+                    return new WaitUntilNextFrame();
+            }
+        }
 
-		private static readonly Dictionary<Coroutine, object?> coroutines = new Dictionary<Coroutine, object?>();
-	}
+        private static readonly Dictionary<Coroutine, object?> coroutines = new Dictionary<Coroutine, object?>();
+    }
 }

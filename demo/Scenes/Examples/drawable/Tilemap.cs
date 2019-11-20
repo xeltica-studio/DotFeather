bccdb3e524971a66f25a8df4b62fb60fb92af77f
Xeltica <xeltica@gmail.com>
2019-11-20 16:16:25 +0900

    タイルマップのデモ

--- demo/Scenes/Examples/drawable/Tilemap.cs
+++ demo/Scenes/Examples/drawable/Tilemap.cs
@@ -1,3 +1,6 @@
+using System.Collections;
+using System.Drawing;
+
 namespace DotFeather.Demo
 {
     [DemoScene("/drawable/tilemap")]
@@ -7,13 +10,57 @@ namespace DotFeather.Demo
     {
         public override void OnStart(Router router, GameBase game, System.Collections.Generic.Dictionary<string, object> args)
         {
-            game.Print("Press ESC to return");
+            game.StartCoroutine(Main(game));
         }
 
         public override void OnUpdate(Router router, GameBase game, DFEventArgs e)
         {
             if (DFKeyboard.Escape.IsKeyUp)
                 router.ChangeScene<LauncherScene>();
+        }
+
+        IEnumerator Main(GameBase game)
+        {
+            var tile = Tile.LoadFrom("./ichigo.png");
+            var map = new Tilemap(Vector.One * 16);
+            Root.Add(map);
+            game.Print("Initialized a tilemap.");
+            yield return new WaitForSeconds(1);
+
+            game.Print("Put tiles randomly...");
+            for (var i = 0; i < 512; i++)
+            {
+                map.SetTile(
+                    // Determine the random position
+                    Random.NextVectorInt(game.Width / 16, game.Height / 16),
+                    tile,
+                    // Specify tint color with 50% probability
+                    Random.Next(10) < 5 ? default(Color?) : Random.NextColor()
+                );
+                if (i % 4 == 0)
+                    yield return null;
+            }
+            yield return new WaitForSeconds(0.8f);
+
+            game.Print("Set map's location to (64, 64)");
+            map.Location = Vector.One * 64;
+            yield return new WaitForSeconds(1);
+
+            game.Print("Set map's scale to (0.5, 0.5)");
+            map.Scale = Vector.One * 0.5f;
+            yield return new WaitForSeconds(1);
+
+            map.Clear();
+            map.Line(0, 0, game.Width / 16, game.Height / 16, tile);
+            game.Print("Drew line.");
+            yield return new WaitForSeconds(1);
+
+            map.Clear();
+            map.Fill(3, 6, 24, 16, tile);
+            game.Print("Filled.");
+            yield return new WaitForSeconds(1);
+
+            game.Print("Press ESC to return");
         }
     }
 

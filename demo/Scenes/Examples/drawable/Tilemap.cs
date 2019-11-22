7724a82a0e490ef9c77294da77513d73a8f4b6a9
Xeltica <xeltica@gmail.com>
2019-11-22 18:56:25 +0900

    細かい変更

--- demo/Scenes/Examples/drawable/Tilemap.cs
+++ demo/Scenes/Examples/drawable/Tilemap.cs
@@ -5,7 +5,7 @@ namespace DotFeather.Demo
 {
 	[DemoScene("/drawable/tilemap")]
 	[Description("en", "Generate tilemap and scroll")]
-	[Description("ja", "タイルマップを作成しスクロールします")]
+	[Description("ja", "タイルマップを作成し動かします")]
 	public class TilemapExampleScene : Scene
 	{
 		public override void OnStart(Router router, GameBase game, System.Collections.Generic.Dictionary<string, object> args)

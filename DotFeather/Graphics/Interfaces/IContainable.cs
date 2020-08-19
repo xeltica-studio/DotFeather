d97ca84980ec9f70bf0f832d937cf96e71d45cd1
Xeltica <xeltica@gmail.com>
2020-08-19 14:18:31 +0900

    不要なファイルの削除

--- DotFeather/Graphics/Interfaces/IContainable.cs
+++
@@ -1,8 +0,0 @@
-namespace DotFeather
-{
-	public interface IContainable : IDrawable
-	{
-		IContainable? Parent { get; internal set; }
-		Vector AbsoluteLocation => Location + (Parent?.AbsoluteLocation ?? Vector.Zero);
-	}
-}

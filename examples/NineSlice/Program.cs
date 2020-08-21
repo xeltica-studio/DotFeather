ce491db15f37eaf207df63637aa33254742fe3ad
Xeltica <xeltica@gmail.com>
2020-08-21 14:33:31 +0900

    サンプルの更新

--- examples/NineSlice/Program.cs
+++ examples/NineSlice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using DotFeather;
 
 namespace NineSlice
@@ -9,8 +10,14 @@ namespace NineSlice
 		{
 			DF.Root.Add(
 				new Element("main",
-					new Element("sprite").With(sprite = new SpriteRenderer("rect.png")),
-					new Element("9slice").With(nineslice = new NineSliceSpriteRenderer("rect.png", 16, 16, 16, 16))
+					new Element("sprite")
+						.With(sprite = new SpriteRenderer("rect.png")),
+					new Element("9slice")
+						.With(nineslice = new NineSliceSpriteRenderer("rect.png", 16, 16, 16, 16)),
+					new Element("text1")
+						.With(t1 = new TextRenderer("Sprite", DFFont.GetDefault(18), Color.Lime)),
+					new Element("text2")
+						.With(t2 = new TextRenderer("9-slice Sprite", DFFont.GetDefault(18), Color.Lime))
 				)
 			);
 
@@ -25,13 +32,16 @@ namespace NineSlice
 			sprite.Transform.Location = (DF.Window.Width / 4 - 128, 64);
 			nineslice.Transform.Location = (DF.Window.Width / 4 + 32, 64);
 
+			t1.Transform.Location = (sprite.Transform.Location.X, sprite.Transform.Location.Y - 24);
+			t2.Transform.Location = (nineslice.Transform.Location.X, nineslice.Transform.Location.Y - 24);
+
 			sprite.Width = nineslice.Width = (int)(64 + 64 * Math.Abs(Math.Sin(Time.Now * 2)));
 			sprite.Height = nineslice.Height = (int)(64 + 256 * Math.Abs(Math.Sin(Time.Now * 2)));
 		}
 
 		private static SpriteRenderer sprite;
 		private static NineSliceSpriteRenderer nineslice;
-		// private static readonly TextDrawable t1 = new TextDrawable("Sprite", DFFont.GetDefault(18), Color.Lime);
-		// private static readonly TextDrawable t2 = new TextDrawable("9-slice Sprite", DFFont.GetDefault(18), Color.Lime);
+		private static TextRenderer t1;
+		private static TextRenderer t2;
 	}
 }

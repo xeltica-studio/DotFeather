da5c269d849f6d9453dfc46fece1b8d66340a00d
Xeltica <xeltica@gmail.com>
2020-08-22 21:28:25 +0900

    SpriteAnimator

---
+++ examples/Demo/Scenes/Examples/graphics/SpriteAnimator.cs
@@ -0,0 +1,35 @@
+namespace DotFeather.Demo
+{
+	[DemoScene("/graphics/sprite animator")]
+	[Description("en", "Usage of SpriteAnimator component")]
+	[Description("ja", "Sprite Animator コンポーネントの使用例")]
+	public class SpriteAniamtorExampleScene : Scene
+	{
+		public override void OnStart(System.Collections.Generic.Dictionary<string, object> _)
+		{
+			qbox = Texture2D.LoadAndSplitFrom("qbox.png", 8, 1, (16, 16));
+			Root.Add(
+				new Element("Question Box")
+					.With((128, 128))
+					.With(new SpriteRenderer())
+					.With(new SpriteAnimator(qbox, true, -1, 8))
+			);
+			DF.Console.Print("Press ESC to return");
+		}
+
+		public override void OnUpdate()
+		{
+			if (DFKeyboard.Escape.IsKeyDown)
+				DF.Router.ChangeScene<LauncherScene>();
+		}
+
+		public override void OnDestroy()
+		{
+			if (qbox != null)
+				foreach (var t in qbox)
+					t.Dispose();
+		}
+
+		private Texture2D[]? qbox;
+	}
+}
